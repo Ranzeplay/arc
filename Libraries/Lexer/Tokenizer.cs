1fa7cce78a47842569190e9dcbbf3d1872b0f9cd
Jeb Feng <ranzeplay@outlook.com>
2023-01-25 22:51:34 +0800

    feat (compiler/lexer) : Implement tokenizer

--- Libraries/Lexer/Tokenizer.cs
+++ Libraries/Lexer/Tokenizer.cs
@@ -1,7 +1,115 @@
-﻿namespace Arc.Compiler.Lexer
+﻿using Arc.Compiler.Lexer.Rules;
+using Arc.Compiler.Shared.Compilation;
+using Arc.Compiler.Shared.LexicalAnalysis;
+
+namespace Arc.Compiler.Lexer
 {
     public class Tokenizer
     {
+        public static TokenizedFile Tokenize(SourceFile source, bool trimWhitespace)
+        {
+            var tokens = new List<Token>();
 
+            int currentIndex = 0;
+            while (currentIndex < source.Content.Length)
+            {
+                var comment = Comment.Build(source, currentIndex);
+                if (comment is not null)
+                {
+                    if (comment.Section is not null)
+                    {
+                        tokens.Add(comment.Section);
+                        currentIndex += comment.Length;
+                    }
+                }
+
+                var semicolon = Semicolon.Build(source, currentIndex);
+                if (semicolon is not null)
+                {
+                    if (semicolon.Section is not null)
+                    {
+                        tokens.Add(semicolon.Section);
+                        currentIndex += semicolon.Length;
+                    }
+                }
+
+                var keyword = Keyword.Build(source, currentIndex);
+                if (keyword is not null)
+                {
+                    if (keyword.Section is not null)
+                    {
+                        tokens.Add(keyword.Section);
+                        currentIndex += keyword.Length;
+                    }
+                }
+
+                var identifier = Identifier.Build(source, currentIndex);
+                if (identifier is not null)
+                {
+                    if (identifier.Section is not null)
+                    {
+                        tokens.Add(identifier.Section);
+                        currentIndex += identifier.Length;
+                    }
+                }
+
+                var op = Operator.Build(source, currentIndex);
+                if (op is not null)
+                {
+                    if (op.Section is not null)
+                    {
+                        tokens.Add(op.Section);
+                        currentIndex += op.Length;
+                    }
+                }
+
+                var number = Number.Build(source, currentIndex);
+                if (number is not null)
+                {
+                    if (number.Section is not null)
+                    {
+                        tokens.Add(number.Section);
+                        currentIndex += number.Length;
+                    }
+                }
+
+                var str = Str.Build(source, currentIndex);
+                if (str is not null)
+                {
+                    if (str.Section is not null)
+                    {
+                        tokens.Add(str.Section);
+                        currentIndex += str.Length;
+                    }
+                }
+
+                var container = Container.Build(source, currentIndex);
+                if (container is not null)
+                {
+                    if (container.Section is not null)
+                    {
+                        tokens.Add(container.Section);
+                        currentIndex += container.Length;
+                    }
+                }
+
+                var whitespace = Whitespace.Build(source, currentIndex);
+                if (whitespace is not null)
+                {
+                    if (whitespace.Section is not null)
+                    {
+                        if (!trimWhitespace)
+                        {
+                            tokens.Add(whitespace.Section);
+                        }
+
+                        currentIndex += whitespace.Length;
+                    }
+                }
+
+            }
+
+            return new(source, tokens.ToArray());
+        }
     }
 }
---
+++ Tests/LexicalAnalysis/TokenizerTest.cs
@@ -0,0 +1,28 @@
+﻿using Arc.Compiler.Lexer;
+using Arc.Compiler.Lexer.Rules;
+using Arc.Compiler.Shared.Compilation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arc.Compiler.Tests.LexicalAnalysis
+{
+    [Category("LexicalAnalysis")]
+    public class TokenizerTest
+    {
+        [SetUp]
+        public void Setup(){}
+
+        [Test, MaxTime(2000)]
+        public void SimpleTest()
+        {
+            var text = "export func main() { std::io::println(\"123456\"); return 0; }";
+            var source = new SourceFile("test", text);
+            var result = Tokenizer.Tokenize(source, true);
+
+            Assert.That(result.Tokens, Has.Length.EqualTo(19));
+        }
+    }
+}
