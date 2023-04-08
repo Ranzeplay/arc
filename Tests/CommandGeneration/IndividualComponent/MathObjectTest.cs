5f9594e429aca600570732ef56de535b0bb8715c
Jeb Feng <ranzeplay@outlook.com>
2023-04-08 23:51:48 +0800

    test : Rearrange command generation test directory structure

--- Tests/CommandGeneration/IndividualComponent/MathObjectTest.cs
+++ Tests/CommandGeneration/IndividualComponent/MathObjectTest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Arc.Compiler.Tests.IndividualComponent
+namespace Arc.Compiler.Tests.CommandGeneration.IndividualComponent
 {
     [Category("IndividualComponent")]
     public class MathObjectTest
