415001aae7aa120a775e22d70573c21aa9500c52
Jeb Feng <ranzeplay@outlook.com>
2023-05-28 14:05:36 +0800

    refr (parser/expression) : Extract `SimpleExpression.ToPostfixExpression` as an extension of class `SimpleExpression`

---
+++ Libraries/Shared/Parsing/Components/Expression/Extensions.cs
@@ -0,0 +1,79 @@
+﻿using Arc.Compiler.Shared.LexicalAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arc.Compiler.Shared.Parsing.Components.Expression
+{
+    public static class Extensions
+    {
+        public static SimpleExpression ToPostfixExpression(this SimpleExpression expr)
+        {
+            var operatorStack = new Stack<ExpressionTerm>();
+            var result = new List<ExpressionTerm>();
+
+            foreach (var term in expr.Terms)
+            {
+                switch (term.TermType)
+                {
+                    case ExpressionTermType.Data:
+                        {
+                            // Push all terms into result directly (infix to postfix)
+                            result.Add(term);
+                            break;
+                        }
+                    case ExpressionTermType.Operator:
+                        {
+                            // The previous TermType::Priority must increased the priority level
+                            while (operatorStack.TryPeek(out var r) && r.TermType == ExpressionTermType.Operator)
+                            {
+                                // Pop if operator priority is higher than current operator
+                                if (TokenConstants.CalculationOperatorPriority[r.GetOperator()!.CalculationOperator] > TokenConstants.CalculationOperatorPriority[term.GetOperator()!.CalculationOperator])
+                                {
+                                    result.Add(operatorStack.Pop());
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                            operatorStack.Push(term);
+
+                            break;
+                        }
+                    case ExpressionTermType.OpenPriority:
+                        {
+                            operatorStack.Push(term);
+                            break;
+                        }
+                    case ExpressionTermType.ClosePriority:
+                        {
+                            // Pop until bracket
+                            while (operatorStack.TryPeek(out var r) && r.TermType == ExpressionTermType.Operator)
+                            {
+                                result.Add(operatorStack.Pop());
+
+                            }
+
+                            // Pop this bracket (it won't be transferred to result)
+                            operatorStack.Pop();
+
+                            break;
+                        }
+                    default:
+                        {
+                            throw new NotImplementedException("Invalid ExpressionTermType");
+                        }
+                }
+            }
+
+            // Push all operators remained
+            result.AddRange(operatorStack.ToArray());
+
+            return new(result.ToArray(), expr.OutputDataType);
+        }
+    }
+}
--- Tests/CommandGeneration/ExpressionTest.cs
+++ Tests/CommandGeneration/ExpressionTest.cs
@@ -3,6 +3,7 @@ using Arc.Compiler.Parser.Builders.Components.Expression;
 using Arc.Compiler.Shared.CommandGeneration;
 using Arc.Compiler.Shared.Compilation;
 using Arc.Compiler.Shared.Parsing.Components.Data;
+using Arc.Compiler.Shared.Parsing.Components.Expression;
 using Arc.Compiler.Shared.Parsing.Components.Function;
 using Arc.CompilerCommandGenerator.Builders;
 
