0c610413b835f9c668a93eb63f30c7c0f8b80217
Jeb Feng <ranzeplay@outlook.com>
2023-07-02 16:34:25 +0800

    fix (cmdec) : Make UI work

--- Executables/Cmdec/CommandListWindow.cs
+++ Executables/Cmdec/CommandListWindow.cs
@@ -11,27 +11,43 @@ namespace Arc.Cmdec
 
             // Add table
             var table = new DataTable("Table");
-            table.Columns.Add("Position");
+            table.Columns.Add("Location");
             table.Columns.Add("Commands");
             table.Columns.Add("Description");
 
             // Add rows
             foreach (var command in Memory.Package!.Commands)
             {
-                table.Rows.Add(command.Location, command.RawData, command.Description);
+                string rawData = "";
+                foreach (var item in command.RawData)
+                {
+                    var str = Convert.ToString(item, 16).PadLeft(2, '0');
+
+                    rawData += str + ' ';
+                }
+
+                table.Rows.Add($"0x{Convert.ToString(command.Location, 16).PadLeft(8, '0')}", rawData, command.Description);
             }
 
             // Create table
-            Table = new TableView()
+            Table = new TableView
             {
                 X = 0,
                 Y = 0,
                 Width = Dim.Fill(),
-                Height = Dim.Fill(1)
+                Height = Dim.Fill(-1),
+                Table = table,
+                ColumnOffset = 0,
             };
 
-            Table.Data = table;
-            Table.Update();
+            
+            Table.Style.GetOrCreateColumnStyle(table.Columns["Location"])
+                .RepresentationGetter = (v) => v.ToString();
+            Table.Style.GetOrCreateColumnStyle(table.Columns["Commands"])
+                .RepresentationGetter = (v) => v.ToString();
+            Table.Style.GetOrCreateColumnStyle(table.Columns["Description"])
+                .RepresentationGetter = (v) => v.ToString();
+
 
             // Add table to window
             Add(Table);
