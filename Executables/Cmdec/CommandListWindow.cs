aaddbd5d205e51d7c22152bffd0f7e5af8a4b82f
Jeb Feng <ranzeplay@outlook.com>
2023-07-08 15:44:05 +0800

    feat (cmdec) : Add package metadata dialog

--- Executables/Cmdec/CommandListWindow.cs
+++ Executables/Cmdec/CommandListWindow.cs
@@ -40,7 +40,7 @@ namespace Arc.Cmdec
                 ColumnOffset = 0,
             };
 
-            
+
             Table.Style.GetOrCreateColumnStyle(table.Columns["Location"])
                 .RepresentationGetter = (v) => v.ToString();
             Table.Style.GetOrCreateColumnStyle(table.Columns["Commands"])
@@ -48,11 +48,50 @@ namespace Arc.Cmdec
             Table.Style.GetOrCreateColumnStyle(table.Columns["Description"])
                 .RepresentationGetter = (v) => v.ToString();
 
-
             // Add table to window
             Add(Table);
+
+            var tips = new Label
+            {
+                Text = "Ctrl+Q : Quit application / M : View package metadata",
+                X = 0,
+                Y = Pos.Bottom(this) - 3
+            };
+            Add(tips);
         }
 
         public TableView Table { get; set; }
+
+        public override bool OnKeyDown(KeyEvent keyEvent)
+        {
+            if (keyEvent.Key == Key.M || keyEvent.Key == Key.m)
+            {
+                Dialog dialog = new()
+                {
+                    Title = "Package metadata",
+                    Height = Dim.Percent(70),
+                    Width = Dim.Percent(70),
+                    VerticalTextAlignment = VerticalTextAlignment.Justified
+                };
+
+                dialog.Add(new Label(1, 1, $"Package type        :   {Memory.Package!.Metadata.PackageType}"));
+                dialog.Add(new Label(1, 2, $"Data alignment      :   {Memory.Package!.Metadata.DataAlignment}"));
+                dialog.Add(new Label(1, 3, $"Data slot alignment :   {Memory.Package!.Metadata.DataSlotAlignment}"));
+                dialog.Add(new Label(1, 4, $"Address alignment   :   {Memory.Package!.Metadata.AddressAlignment}"));
+                dialog.Add(new Label(1, 5, $"Entry function id   :   {Memory.Package!.Metadata.EntryFunctionId}"));
+                dialog.Add(new Label(1, 6, $"Data section size   :   {Memory.Package!.Metadata.DataSectionSize}"));
+
+                var closeButton = new Button("Close", true);
+                closeButton.Clicked += () =>
+                {
+                    Application.RequestStop();
+                };
+                dialog.AddButton(closeButton);
+
+                Application.Run(dialog);
+            }
+
+            return base.OnKeyDown(keyEvent);
+        }
     }
 }
