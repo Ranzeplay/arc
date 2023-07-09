39378e90cf51778517efc0317c4612ac544a36b3
Jeb Feng <ranzeplay@outlook.com>
2023-07-10 00:10:14 +0800

    feat (cmd_gen/builders) : Add RelativeLocationCommand command builder

---
+++ Libraries/CommandGenerator/Builders/Fragments/RelativeLocationCommand.cs
@@ -0,0 +1,21 @@
+﻿using Arc.Compiler.Shared.CommandGeneration;
+using Arc.CompilerCommandGenerator.Models;
+
+namespace Arc.CompilerCommandGenerator.Builders.Fragments
+{
+    internal class RelativeLocationCommand
+    {
+        public static PartialGenerationResult? Build(GenerationContext<long> source)
+        {
+            var commands = new List<byte>
+            {
+                (byte)(source.Component >= 0 ? 0x0F : 0x0B)
+            };
+
+            var absLocation = PackageMetadata.GenerateDataAligned(long.Abs(source.Component), source.PackageMetadata.AddressAlignment);
+            commands.AddRange(absLocation);
+
+            return new PartialGenerationResult(commands);
+        }
+    }
+}
