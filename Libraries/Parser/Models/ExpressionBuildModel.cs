7d95e09207e567a82994e024a6a1dcd668d94fdf
Jeb Feng <ranzeplay@outlook.com>
2023-08-10 23:17:36 +0800

    refr (compiler/parser/builders) : Convert model `ExpressionBuildModel` from `record` to `class`

--- Libraries/Parser/Models/ExpressionBuildModel.cs
+++ Libraries/Parser/Models/ExpressionBuildModel.cs
@@ -4,8 +4,21 @@ using Arc.Compiler.Shared.Parsing.Components.Function;
 
 namespace Arc.Compiler.Parser.Models
 {
-    public record ExpressionBuildModel(Token[] Tokens, DataDeclarator[] DeclaredData, FunctionDeclarator[] DeclaredFunctions)
+    public class ExpressionBuildModel
     {
+        public Token[] Tokens { get; set; } 
+        
+        public DataDeclarator[] DeclaredData { get; set; } 
+
+        public FunctionDeclarator[] DeclaredFunctions { get; set; }
+
+        public ExpressionBuildModel(Token[] tokens, DataDeclarator[] declaredData, FunctionDeclarator[] declaredFunctions)
+        {
+            Tokens = tokens;
+            DeclaredData = declaredData;
+            DeclaredFunctions = declaredFunctions;
+        }
+
         public ExpressionBuildModel SkipTokens(int count)
         {
             return new(Tokens[count..], DeclaredData, DeclaredFunctions);
