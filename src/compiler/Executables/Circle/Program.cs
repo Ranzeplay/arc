04850adefd36edfdd834d0f1d7b43fe063c3151e
Jeb Feng <ranzeplay@outlook.com>
2024-12-02 16:23:40 +0800

    feat (compiler/cli) : Simple compiler CLI

--- src/compiler/Executables/Circle/Program.cs
+++ src/compiler/Executables/Circle/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Arc compiler CLI");
+﻿using Arc.Compiler.PackageGenerator;
+using Arc.Compiler.PackageGenerator.Models.Descriptors;
+using Arc.Compiler.SyntaxAnalyzer;
+using Arc.Compiler.SyntaxAnalyzer.Models;
+
+Console.WriteLine("Arc compiler CLI");
+
+var text = File.ReadAllText(args[0]);
+var compilationUnit = AntlrAdapter.ParseCompilationUnit(text);
+var syntaxUnit = new ArcCompilationUnit(compilationUnit, args[0]);
+var context = Flow.GenerateUnit(syntaxUnit);
+
+context.PackageDescriptor = new ArcPackageDescriptor()
+{
+    Type = ArcPackageType.Executable,
+    Name = "Test",
+    Version = 0,
+    RootGroupTableEntryPos = 0,
+    RootFunctionTableEntryPos = 0,
+    RootConstantTableEntryPos = 0,
+    RegionTableEntryPos = 0,
+    EntrypointFunctionId = 0,
+    DataAlignmentLength = 8
+};
+
+var outputStream = Flow.DumpFullByteStream(context);
+
+File.WriteAllBytes(args[1], outputStream.ToArray());
