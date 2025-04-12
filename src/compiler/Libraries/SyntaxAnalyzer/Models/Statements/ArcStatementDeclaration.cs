1c203f315112d44fe53b9cc79c039f88e4cc10a1
Jeb Feng <ranzeplay@outlook.com>
2025-04-12 13:58:05 +0800

    feat (compiler/syntax_analyzer) : Assignment on data declaration

--- src/compiler/Libraries/SyntaxAnalyzer/Models/Statements/ArcStatementDeclaration.cs
+++ src/compiler/Libraries/SyntaxAnalyzer/Models/Statements/ArcStatementDeclaration.cs
@@ -1,12 +1,15 @@
 ﻿using Arc.Compiler.SyntaxAnalyzer.Generated.ANTLR;
 using Arc.Compiler.SyntaxAnalyzer.Interfaces;
 using Arc.Compiler.SyntaxAnalyzer.Models.Components;
+using Arc.Compiler.SyntaxAnalyzer.Models.Expression;
 
 namespace Arc.Compiler.SyntaxAnalyzer.Models.Statements
 {
     public class ArcStatementDeclaration(ArcSourceCodeParser.Arc_stmt_declContext context) : ArcExecutionStepBase, IArcTraceable<ArcSourceCodeParser.Arc_stmt_declContext>
     {
         public ArcDataDeclarator DataDeclarator { get; set; } = new(context.arc_data_declarator());
+
+        public ArcExpression? InitialValueExpression { get; set; } = context.arc_expression() != null ? new ArcExpression(context.arc_expression()) : null;
 
         public ArcSourceCodeParser.Arc_stmt_declContext Context { get; } = context;
     }
