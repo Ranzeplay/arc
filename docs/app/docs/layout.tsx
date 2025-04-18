e788d87834a4dbac33f1522dcd2a6995ca533a0b
Jeb Feng <ranzeplay@outlook.com>
2025-04-18 15:02:11 +0800

    docs : Migrate to fumadocs

---
+++ docs/app/docs/layout.tsx
@@ -0,0 +1,46 @@
+import { DocsLayout, DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
+import type { ReactNode } from 'react';
+import { source } from '@/lib/source';
+import { baseOptions } from '../layout.config';
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [],
+  sidebar: {
+    tabs: {
+      transform(option, node) {
+        const meta = source.getNodeMeta(node);
+        if (!meta) return option;
+
+        const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`;
+
+        return {
+          ...option,
+          icon: (
+            <div
+              className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-5"
+              style={
+                {
+                  color,
+                  border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
+                  '--tw-ring-color': `color-mix(in oklab, ${color} 20%, transparent)`,
+                } as object
+              }
+            >
+              {node.icon}
+            </div>
+          ),
+        };
+      },
+    },
+  },
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <DocsLayout {...docsOptions} githubUrl='https://github.com/Ranzeplay/arc'>
+      {children}
+    </DocsLayout>
+  );
+}
