ccec571da42b24a05fb193c5604eae9031e56767
Jeb Feng <ranzeplay@outlook.com>
2025-08-28 23:39:13 +0800

    feat (runtime/stdlib) : Add functions `0xc5` and `0xc6`

--- src/runtime/stdlib/src/array.rs
+++ src/runtime/stdlib/src/array.rs
@@ -1,10 +1,13 @@
-use arc_shared::models::encodings::data_type_enc::{DataTypeEncoding, MemoryStorageType, Mutability};
+use arc_bindings::{arc_function_id, arc_scope_dispatcher};
+use arc_shared::base_type_id::{INTEGER_TYPE_ID, NONE_TYPE_ID};
+use arc_shared::models::encodings::data_type_enc::{
+    DataTypeEncoding, MemoryStorageType, Mutability,
+};
 use arc_shared::models::execution::data::{DataValue, DataValueType};
 use arc_shared::models::execution::result::FunctionExecutionResult;
+use std::any::TypeId;
 use std::cell::RefCell;
 use std::rc::Rc;
-use arc_bindings::{arc_function_id, arc_scope_dispatcher};
-use arc_shared::base_type_id::INTEGER_TYPE_ID;
 
 pub struct ArcStdArray {}
 
@@ -53,7 +56,7 @@ impl ArcStdArray {
                     };
 
                     arr.push(integer_value);
-                },
+                }
                 _ => return Err("Expected integer".to_string()),
             },
             _ => return Err("Expected array".to_string()),
@@ -102,6 +105,71 @@ impl ArcStdArray {
                 ))))
             }
             _ => Err("Expected array".to_string()),
+        }
+    }
+
+    #[arc_function_id(0xc5)]
+    pub fn resize_int_array(
+        args: &mut Vec<Rc<RefCell<DataValue>>>,
+    ) -> Result<FunctionExecutionResult, String> {
+        let capacity = args.pop().unwrap();
+        let array = args.pop().unwrap();
+
+        let mut array = array.borrow_mut();
+
+        match &mut array.value {
+            DataValueType::Array(array) => match &capacity.borrow().value {
+                DataValueType::Integer(i) => {
+                    let i = *i as usize;
+                    array.resize(
+                        i,
+                        Rc::new(RefCell::new(DataValue {
+                            data_type: DataTypeEncoding {
+                                type_id: *NONE_TYPE_ID,
+                                dimension: 0,
+                                mutability: Mutability::Mutable,
+                                memory_storage_type: MemoryStorageType::Reference,
+                            },
+                            value: DataValueType::None,
+                        })),
+                    );
+                }
+                _ => return Err("Expected integer".to_string()),
+            },
+            _ => return Err("Expected array".to_string()),
+        }
+
+        Ok(FunctionExecutionResult::Success(None))
+    }
+
+    #[arc_function_id(0xc6)]
+    pub fn create_int_array_p(
+        args: &mut Vec<Rc<RefCell<DataValue>>>,
+    ) -> Result<FunctionExecutionResult, String> {
+        let size = args.pop().unwrap();
+
+        let size = size.borrow();
+
+        match &size.value {
+            DataValueType::Integer(i) => {
+                let i = *i as usize;
+
+                let data_type = DataTypeEncoding {
+                    type_id: *INTEGER_TYPE_ID,
+                    dimension: 1,
+                    mutability: Mutability::Mutable,
+                    memory_storage_type: MemoryStorageType::Value,
+                };
+
+                let value = DataValueType::Array(Vec::with_capacity(i));
+
+                let data = DataValue { data_type, value };
+
+                Ok(FunctionExecutionResult::Success(Some(Rc::new(
+                    RefCell::new(data),
+                ))))
+            }
+            _ => Err("Expected integer".to_string()),
         }
     }
 }
