ba3a0e6e9b46714f3019f0fbefb137177534bd38
Jeb Feng <ranzeplay@outlook.com>
2025-10-17 16:33:48 +0800

    feat (compiler/syntax_analyzer) : Derive ArcNamelessFunction for class
    For better structure

--- src/compiler/Libraries/SyntaxAnalyzer/Models/Components/ArcLambdaExpression.cs
+++ src/compiler/Libraries/SyntaxAnalyzer/Models/Components/ArcLambdaExpression.cs
@@ -4,11 +4,19 @@ using Arc.Compiler.SyntaxAnalyzer.Models.Function;
 
 namespace Arc.Compiler.SyntaxAnalyzer.Models.Components;
 
-public class ArcLambdaExpression(ArcSourceCodeParser.Arc_lambda_expressionContext context)
+public class ArcLambdaExpression : ArcNamelessFunction<ArcSourceCodeParser.Arc_lambda_expressionContext>
 {
-    public IEnumerable<ArcFunctionArgument> Parameters { get; set; } = context.arc_wrapped_arg_list().arc_arg_list()?.arc_data_declarator().Select(arg => new ArcFunctionArgument(arg)) ?? [];
-
-    public ArcDataType ReturnType { get; set; } = new(context.arc_data_type());
-    
-    public ArcSourceCodeParser.Arc_lambda_expressionContext Context { get; set; } = context;
+    public ArcLambdaExpression(ArcSourceCodeParser.Arc_lambda_expressionContext context)
+    {
+        Context = context;
+        Declarator = new ArcNamelessFunctionDeclarator
+        {
+            ReturnType = new ArcDataType(context.arc_data_type()),
+            Arguments = context.arc_wrapped_arg_list()?
+                .arc_arg_list()
+                .arc_data_declarator()
+                .Select(d => new ArcFunctionArgument(d)) ?? []
+        };
+        Body = new ArcFunctionBody(context.arc_wrapped_function_body());
+    }
 }
