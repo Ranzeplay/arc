fbd66578a16321c808621e79f3dc338e7bf48c6b
Jeb Feng <ranzeplay@outlook.com>
2025-10-19 16:57:41 +0800

    feat (docs) : Update landing page and footer

--- docs/app/components/footer.tsx
+++ docs/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="py-5 flex flex-row items-baseline justify-between px-12 bg-neutral-200 text-neutral-800 mt-auto">
         <p>
-          Copyright &copy; {process.env.NEXT_PUBLIC_COPYRIGHT_YEAR} -
+          Copyright &copy; 2023-{process.env.NEXT_PUBLIC_COPYRIGHT_YEAR} -
           The Arc Programming Language
         </p>
         {process.env.NEXT_PUBLIC_NODE_ENV == "production" ? (
